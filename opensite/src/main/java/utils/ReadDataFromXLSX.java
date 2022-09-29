3cd779a8d8d40a5b2fb026659565d734ffd51896
shivachiluveri <shiva.chiluveri@gmail.com>
2022-09-29 23:59:47 +0530

    com to local repo

--- opensite/src/main/java/utils/ReadDataFromXLSX.java
+++ opensite/src/main/java/utils/ReadDataFromXLSX.java
@@ -1,5 +1,7 @@
 package utils;
 
 public class ReadDataFromXLSX {
+	
+	
 
 }
--- opensite/src/test/java/apachePOI/RradDataFromxls.java
+++ opensite/src/test/java/apachePOI/RradDataFromxls.java
@@ -1,5 +1,7 @@
 package apachePOI;
 
+import java.util.Random;
+
 public class RradDataFromxls {
 
 	public static void main(String[] args) {
@@ -7,4 +9,29 @@ public class RradDataFromxls {
 
 	}
 
+	public String name() {
+		String fn;
+		String text = "abcdefghijklmnopqrstuvwxyz";
+		StringBuilder salt = new StringBuilder();
+		Random rnd = new Random();
+		while (salt.length() < 4) {
+			int index = (int) (rnd.nextFloat() * text.length());
+			salt.append(text.charAt(index));
+		}
+		fn = salt.toString();
+		return fn;
+	}
+
+	public String phoneNum() {
+		String phone;
+		String nums = "0123456789";
+		StringBuilder salt1 = new StringBuilder();
+		Random rnd = new Random();
+		while (salt1.length() < 10) {
+			int index = (int) (rnd.nextFloat() * nums.length());
+			salt1.append(nums.charAt(index));
+		}
+		phone = salt1.toString();
+		return phone;
+	}
 }
